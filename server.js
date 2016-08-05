5f4fda276e36a4e238ee5623b18c8084d8fc3b7d
sonvq <vuquangson1610@gmail.com>
2016-08-05 14:03:30 +0700

    commit initial code

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var router = express.Router();
+
+/* Creating POOL MySQL connection.*/
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({// to support URL-encoded bodies
+    extended: true
+}));
+
+router.get('/', function (req, res) {
+    console.log('A user is connected on the web');
+    res.sendStatus(201);
+});
+
+app.post('/send-push', function (req, res) {
+    console.log('A push notification has been sent!');
+    console.log(req.body);
+    var name = req.body.name;
+    var message = req.body.message;
+    var channel_ids = req.body.channel_ids;
+    console.log(channel_ids);
+    io.emit("notify everyone", {user: name, comment: message});
+    console.log('done sent')
+    res.sendStatus(200);
+});
+
+app.use('/', router);
+
+io.on('connection', function (socket) {
+    console.log('A user is connected with socket.io');
+    socket.on('comment added', function (data) {
+        io.emit("notify everyone", {user: data.user, comment: data.comment});
+    });
+});
+
+
+http.listen(8080, '172.16.10.140');
+console.log('Server running at http://172.16.10.140:8080/');
+
